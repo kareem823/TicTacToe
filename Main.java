8f513519ee9a0ac1d04ded154d236f3ff464218a
slinkyGirrage <92062262+kareem823@users.noreply.github.com>
2022-07-03 22:22:16 -0400

    main for ttt

---
+++ Main.java
@@ -0,0 +1,23 @@
+/**
+ * Author: Kareem Idris, S#: 0881393, Ultimate esquire 
+ * Program: Main.java
+ * Purpose:  
+ * Date: Jul 3, 2022
+ */
+
+/**
+ * @author Kareem
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// make the object
+		TicTacToe tictactoe = new TicTacToe();
+		
+	}
+
+}
